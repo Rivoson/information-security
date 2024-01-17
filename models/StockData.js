d360b14616e889df5275b432c2ea65b076eba7c7
radoniaina <rado@hairun-technology.fr>
2024-01-17 08:46:23 +0100

    init stock checker

---
+++ models/StockData.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var StockDataSchema = new Schema(
+  {
+    stock: { type: String },
+    price: { type: String },
+    likes: { type: Number, default: 0 },
+    ips: { type: Array },
+  },
+  { versionKey: false }
+);
+
+var StockData = mongoose.model("StockData", StockDataSchema);
+
+module.exports.StockData = StockData;
