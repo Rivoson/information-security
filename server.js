bdf163650c5528f189d12af88fad1830d2b432ec
radoniaina <rado@hairun-technology.fr>
2024-01-18 09:49:48 +0100

    remove cloudflare

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(
     csp: {
       directives: {
         defaultSrc: ["'self'"],
-        styleSrc: ["'self'", "cdnjs.cloudflare.com"]
-      }
+        styleSrc: ["'self'"],
+      },
     },
-    hidePoweredBy: { setTo: "PHP 4.2.0" }
+    hidePoweredBy: { setTo: "PHP 4.2.0" },
   })
 );
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
-app.route("/").get(function(req, res) {
+app.route("/").get(function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
@@ -42,19 +42,16 @@ fccTestingRoutes(app);
 apiRoutes(app);
 
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res
-    .status(404)
-    .type("text")
-    .send("Not Found");
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
   if (process.env.NODE_ENV === "test") {
     console.log("Running Tests...");
-    setTimeout(function() {
+    setTimeout(function () {
       try {
         runner.run();
       } catch (e) {
