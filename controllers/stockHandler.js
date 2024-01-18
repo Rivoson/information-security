9e0a56a9006e2ea4bb1ef184a969e5f2890cea46
radoniaina <rado@hairun-technology.fr>
2024-01-18 10:41:53 +0100

    add new stock price proxy url

--- controllers/stockHandler.js
+++ controllers/stockHandler.js
@@ -1,16 +1,18 @@
 const axios = require("axios");
 require("dotenv").config();
 
-const getStockPrice = async stockName => {
+const getStockPrice = async (stockName) => {
   //check for valid stockName
   if (stockName === "") throw "Stock symbol cannot be empty";
-  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockName}&interval=5min&apikey=${process.env.STOCK_API}`;
+  const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockName}/quote`;
 
   try {
     //call external api
     const response = await axios.get(url);
     //get data from api
     const stockData = await response.data;
+    console.log(stockData);
+    return parseFloat(stockData["latestPrice"]);
     //filter out today's recent time data
     const todaysStock = stockData["Time Series (5min)"];
     //check for valid response
@@ -20,12 +22,12 @@ const getStockPrice = async stockName => {
     const closingStock = todaysStock[Object.keys(todaysStock)[0]];
     //price of closing stock
     const stockPrice = closingStock["4. close"];
-    return parseFloat(stockPrice);
+    return parseFloat(stockData["latestPrice"]);
   } catch (error) {
     throw error;
   }
 };
 
 module.exports = {
-  getStockPrice
+  getStockPrice,
 };
